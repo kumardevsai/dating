730194f8cd8995d8e3a3b5d74aa961166fd3a4b1
Thịnh Lê <lethinhcdt@gmail.com>
2017-02-27 00:11:35 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(name: "Thinh", email:"lethinhcdt@gmail.com", password: "123456")
+user1 = User.create(name: "Thinh", email:"lethinhcdt@gmail.com", password: "123456", image_url: "https://organicthemes.com/demo/profile/files/2012/12/profile_img.png")
 p user1
 
-user2 = User.create({name: "Thinh Le", email:"tibi01993@gmail.com", password: "654321"})
+user2 = User.create({name: "Thinh Le", email:"tibi01993@gmail.com", password: "654321", image_url: "http://www.hvac2000.com/wp-content/uploads/2016/05/Profile-Pictures-2.jpg"})
 p user2
 
 Message.create!(sender_id: user1.id, receiver_id: user2.id, read_at: nil, title:"unread message", body:"Text message from user1 to user2")
